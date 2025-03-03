d9eb1bbea8f126d66316474e8fd6decd2a6cd6ab
AndreaBruno97 <andbruno97@gmail.com>
2025-03-03 20:33:59 +0100

    Adaptive Screen Size handling - New file

---
+++ app/src/main/java/com/example/recipebook/ui/preview/DevicesPreview.kt
@@ -0,0 +1,13 @@
+package com.example.recipebook.ui.preview
+
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(name = "phone", device = "spec:width=411dp,height=891dp")
+annotation class PhonePreview
+
+@Preview(name = "phone", device = "spec:width=673dp,height=841dp")
+annotation class FoldablePreview
+
+@Preview(name = "tablet", device = "spec:width=1280dp,height=800dp,dpi=240")
+annotation class TabletPreview
